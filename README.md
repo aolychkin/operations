5379df7824a9ded87625c2a58dc30f15eb936489
aolychkin <aolychkin@gmail.com>
2024-09-27 14:30:27 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Запуск программы: go run cmd/sso/main.go --config=./config/local.yaml
+
+TODO: Сделать нормализацию БД
+TODO: Сделать пермишен сервис
+TODO: setUserAdmin handler + not Admin
+
+LEARN: контекст в Go + Background
+LEARN: testify
+LEARN: приватная сеть для общения сервисов
+LEARN: Транзакции баз данных
---
+++ cmd/sso/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"grpc-service-ref/internal/app"
+	"grpc-service-ref/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application", slog.Any("config", cfg))
+
+	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
+
+	go application.GRPCSrv.MustRun()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	signal := <-stop
+
+	log.Info("stopping application", slog.String("signal", signal.String()))
+
+	application.GRPCSrv.Stop()
+
+	log.Info("application stopped")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
